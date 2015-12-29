eb71f6be2b91bbe20a20c270e38f4e8012498f17
naveenlj <naveen09mca@gmail.com>
2015-12-30 02:12:25 +0530

    added root check

--- graphite_installc7.sh
+++ graphite_installc7.sh
@@ -1,5 +1,11 @@
-#/bin/bash
+#!/bin/bash
+# Custom script created by naveenlj
+# Make sure only root can run our script
 
+if [ "$(id -u)" != "0" ]; then
+   echo "This script must be run as root" 1>&2
+   exit 1
+fi
 set -x
 
 which curl >/dev/null 2>&1
